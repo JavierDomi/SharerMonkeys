7bf33d915beba7d98d2c679e237c93e21374990b
Joana <joanacascogalea@gmail.com>
2024-02-20 13:30:47 +0100

    Autentitacion

---
+++ app/src/main/java/com/dam/sharermonkeys/autentication/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.dam.sharermonkeys.autentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dam.sharermonkeys.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
