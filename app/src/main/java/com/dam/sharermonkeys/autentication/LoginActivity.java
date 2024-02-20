15f08f1a920112f1b2cc9f7f9bb6a379a907411a
Joana <joanacascogalea@gmail.com>
2024-02-20 13:33:02 +0100

    Autentitacion

--- app/src/main/java/com/dam/sharermonkeys/autentication/LoginActivity.java
+++ app/src/main/java/com/dam/sharermonkeys/autentication/LoginActivity.java
@@ -1,16 +1,92 @@
 package com.dam.sharermonkeys.autentication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.dam.sharermonkeys.MainActivity;
 import com.dam.sharermonkeys.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    TextInputEditText etEmail;
+    TextInputEditText etPassword;
+    Button btnLogin;
+    ProgressBar progBar;
+    TextView tvNewAccount;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        progBar = findViewById(R.id.progressBar);
+        tvNewAccount = findViewById(R.id.tvNewAccount);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progBar.setVisibility(View.VISIBLE);
+                String email = String.valueOf(etEmail.getText());
+                String password = String.valueOf(etPassword.getText());
+
+                if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Enter both email and password", Toast.LENGTH_SHORT).show();
+                    progBar.setVisibility(View.GONE);
+                    return;
+                }
+                if (TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Enter the email", Toast.LENGTH_SHORT).show();
+                    progBar.setVisibility(View.GONE);
+                    return;
+                }
+                if (!TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+                    Toast.makeText(LoginActivity.this, "Enter the password", Toast.LENGTH_SHORT).show();
+                    progBar.setVisibility(View.GONE);
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Login Successful", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(i);
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        tvNewAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), NewAccountActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/dam/sharermonkeys/autentication/NewAccountActivity.java
+++ app/src/main/java/com/dam/sharermonkeys/autentication/NewAccountActivity.java
@@ -1,16 +1,96 @@
 package com.dam.sharermonkeys.autentication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.dam.sharermonkeys.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NewAccountActivity extends AppCompatActivity {
+    TextInputEditText etEmail;
+    TextInputEditText etPassword;
+    Button btnSignUp;
+    TextView tvLogin;
+    FirebaseAuth mAuth;
+    ProgressBar progBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+        btnSignUp = findViewById(R.id.btnSignUp);
+        progBar = findViewById(R.id.progressBar);
+        tvLogin = findViewById(R.id.tvLogin);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progBar.setVisibility(View.VISIBLE);
+                String email = String.valueOf(etEmail.getText());
+                String password = String.valueOf(etPassword.getText());
+
+                if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+                    // Ambos email y contraseña están vacíos
+                    Toast.makeText(NewAccountActivity.this, R.string.enter_email_password, Toast.LENGTH_SHORT).show();
+                    progBar.setVisibility(View.GONE);
+                    return;
+                }
+                if (TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+                    // El email está vacío, pero la contraseña no
+                    Toast.makeText(NewAccountActivity.this, R.string.enter_email, Toast.LENGTH_SHORT).show();
+                    progBar.setVisibility(View.GONE);
+                    return;
+                }
+                if (!TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+                    // La contraseña está vacía, pero el email no
+                    Toast.makeText(NewAccountActivity.this, R.string.enter_password, Toast.LENGTH_SHORT).show();
+                    progBar.setVisibility(View.GONE);
+                    return;
+                }
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            Toast.makeText(NewAccountActivity.this, R.string.account_created, Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+
+                        } else {
+                            Toast.makeText(NewAccountActivity.this, R.string.account_creation_fail, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
     }
 }
