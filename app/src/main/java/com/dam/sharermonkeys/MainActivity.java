5cc20a0d6850090b2ba855ef61de0469eb979d55
JavierDomi <javi.dominguez.ib@gmail.com>
2024-02-21 20:31:33 +0100

    Menu superior para act main y xml para act main creados

--- app/src/main/java/com/dam/sharermonkeys/MainActivity.java
+++ app/src/main/java/com/dam/sharermonkeys/MainActivity.java
@@ -1,9 +1,17 @@
 package com.dam.sharermonkeys;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.dam.sharermonkeys.autentication.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -15,5 +23,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_activity_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.mnLogOut) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setCancelable(false);
+
+            builder.setPositiveButton(R.string.btn_yes, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(i);
+
+                }
+            });
+
+        } else if (item.getItemId() == R.id.mnExit) {
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setCancelable(false);
+
+            builder.setPositiveButton(R.string.btn_yes, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    finish();
+                    System.exit(0);
+
+                }
+            });
+
+        } else if (item.getItemId() == R.id.mnDelete) {
+
+            Toast.makeText(this, R.string.hold_to_delete, Toast.LENGTH_LONG).show();
+
+            //TODO: Implementar acción para borrar un grupo de gastos (Al dejar pulsado?)
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
