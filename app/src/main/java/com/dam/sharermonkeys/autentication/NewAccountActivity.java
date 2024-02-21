4ad52c4800863a538edfc1fb1b9a6fb914e3b0a1
Liana <lianavi14@gmail.com>
2024-02-21 23:28:12 +0100

    Convalidar datos de registro y xml de login, registro

--- app/src/main/java/com/dam/sharermonkeys/autentication/NewAccountActivity.java
+++ app/src/main/java/com/dam/sharermonkeys/autentication/NewAccountActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -22,6 +23,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class NewAccountActivity extends AppCompatActivity {
     TextInputEditText etEmail;
     TextInputEditText etPassword;
+    TextInputEditText etConfPass;
     Button btnSignUp;
     TextView tvLogin;
     FirebaseAuth mAuth;
@@ -34,6 +36,7 @@ public class NewAccountActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
+        etConfPass= findViewById(R.id.etConfirmPasswordReg);
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
         btnSignUp = findViewById(R.id.btnSignUp);
@@ -43,11 +46,16 @@ public class NewAccountActivity extends AppCompatActivity {
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                progBar.setVisibility(View.VISIBLE);
+                //progBar.setVisibility(View.GONE);
                 String email = String.valueOf(etEmail.getText());
                 String password = String.valueOf(etPassword.getText());
 
-                if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
+                if(!validateEmail(email) || !validatePassword()){
+                    return;
+                }
+
+                // ESTA EL METODO validateEmail y validatePassword
+                /*if (TextUtils.isEmpty(email) && TextUtils.isEmpty(password)) {
                     // Ambos email y contraseña están vacíos
                     Toast.makeText(NewAccountActivity.this, R.string.enter_email_password, Toast.LENGTH_SHORT).show();
                     progBar.setVisibility(View.GONE);
@@ -64,7 +72,7 @@ public class NewAccountActivity extends AppCompatActivity {
                     Toast.makeText(NewAccountActivity.this, R.string.enter_password, Toast.LENGTH_SHORT).show();
                     progBar.setVisibility(View.GONE);
                     return;
-                }
+                }*/
                 mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -92,5 +100,52 @@ public class NewAccountActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private boolean validatePassword() {
+        String password;
+        password = String.valueOf(etPassword.getText());
+        String confPasswod;
+        confPasswod = String.valueOf(etConfPass.getText());
+
+        if(password.isEmpty()){
+            Toast.makeText(NewAccountActivity.this, R.string.enter_password, Toast.LENGTH_SHORT).show();
+            return false;
+        }if (password.length()<5){
+            Toast.makeText(NewAccountActivity.this, R.string.enter_password_char, Toast.LENGTH_SHORT).show();
+            return false;
+
+        }if(!password.equals(confPasswod)) {
+            Toast.makeText(NewAccountActivity.this, R.string.enter_password_noMatch, Toast.LENGTH_SHORT).show();
+            return false;
+
+            //contraseña tiene que tener al menos un numero
+        }if (!password.matches(".*\\d.*")) {
+            // '.*' -> repetido cero o mas veces.
+            // '\\d' -> verifica la presencia de un digito
+            // '.*' -> repetido cero o mas veces.
+            Toast.makeText(NewAccountActivity.this, R.string.et_enter_password_digit, Toast.LENGTH_SHORT).show();
+            return false;
+        }if (!password.matches(".*[a-zA-Z].*")) {
+            // Esto verifica si la contraseña contiene al menos una letra
+            Toast.makeText(NewAccountActivity.this, R.string.et_enter_password_letter, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+            return true;
+
+    }
+
+    private boolean validateEmail(String email) {
+        if(email.isEmpty()){
+            //saler error en editText hasta que no pones correo completo
+            etEmail.setError(getString(R.string.et_error_email));
+            return false;
+        }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            etEmail.setError(getString(R.string.set_error_email_invalid));
+            return false;
+        }else{
+            etEmail.setError(null);
+            return true;
+        }
     }
 }
