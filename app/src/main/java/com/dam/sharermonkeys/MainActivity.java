e7f9056ef7a1ba8d0ff4fdbae80d84c91fe67031
JavierDomi <javi.dominguez.ib@gmail.com>
2024-03-12 13:33:05 +0100

    Filtrado por usuario rv funciona

--- app/src/main/java/com/dam/sharermonkeys/MainActivity.java
+++ app/src/main/java/com/dam/sharermonkeys/MainActivity.java
@@ -17,69 +17,36 @@ import android.widget.Toast;
 
 import com.dam.sharermonkeys.adapterutils.FairShareListAdapter;
 import com.dam.sharermonkeys.autentication.LoginActivity;
-import com.dam.sharermonkeys.pojos.FairShare;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
+import com.dam.sharermonkeys.pojos.User;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String REALTIME_PATH = "https://fairshare-ae0be-default-rtdb.europe-west1.firebasedatabase.app/";
 
     RecyclerView recyclerView;
-    DatabaseReference reference;
     FairShareListAdapter adapter;
-    ArrayList<FairShare> list;
     Button btnAddNew;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        reference = FirebaseDatabase.getInstance(REALTIME_PATH).getReference("FairShares");
-
         btnAddNew = findViewById(R.id.btnAddNew);
         recyclerView = findViewById(R.id.rvList);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        list = new ArrayList<>();
+        Intent intent = getIntent();
+        user = (User) intent.getSerializableExtra("user");
 
-        adapter = new FairShareListAdapter(list, this);
-
-        recyclerView.setAdapter(adapter);
-
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                list.clear();
-
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-
-                    FairShare fairShare = dataSnapshot.getValue(FairShare.class);
-                    list.add(fairShare);
-
-                }
-
-                adapter.notifyDataSetChanged();
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-                Toast.makeText(MainActivity.this, R.string.fairShare_list_cancelled, Toast.LENGTH_SHORT).show();
-
-            }
-        });
+        if (user != null) {
+            adapter = new FairShareListAdapter(user.getFairSharesList(), this);
+            recyclerView.setAdapter(adapter);
+        } else {
+            Toast.makeText(MainActivity.this, R.string.error_fetch_user_main, Toast.LENGTH_SHORT).show();
+        }
 
         btnAddNew.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,18 +59,13 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.main_activity_menu, menu);
-
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-
         if (item.getItemId() == R.id.mnLogOut) {
-
-
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             builder.setCancelable(false);
             builder.setTitle(R.string.tit_dialog_exit);
@@ -111,10 +73,8 @@ public class MainActivity extends AppCompatActivity {
             builder.setPositiveButton(R.string.btn_yes, new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-
                     Intent i = new Intent(MainActivity.this, LoginActivity.class);
                     startActivity(i);
-
                 }
             });
             builder.setNegativeButton(R.string.btn_no, new DialogInterface.OnClickListener() {
@@ -125,14 +85,9 @@ public class MainActivity extends AppCompatActivity {
             });
             AlertDialog dialog = builder.create();
             dialog.show();
-
-            builder.show();
-
         } else if (item.getItemId() == R.id.mnExit) {
-
             AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
             builder.setCancelable(false);
-
             builder.setTitle(R.string.tit_dialog_exit);
             builder.setMessage(R.string.message_dialog_exit);
             builder.setPositiveButton(R.string.btn_yes, new DialogInterface.OnClickListener() {
@@ -150,18 +105,19 @@ public class MainActivity extends AppCompatActivity {
             });
             AlertDialog dialog = builder.create();
             dialog.show();
-
-
-            builder.show();
-
         } else if (item.getItemId() == R.id.mnDelete) {
-
             Toast.makeText(this, R.string.hold_to_delete, Toast.LENGTH_LONG).show();
-
             //TODO: Implementar acción para borrar un grupo de gastos (Al dejar pulsado?)
-
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Actualizar la lista de FairShares si es necesario
+        if (user != null && adapter != null) {
+            adapter.notifyDataSetChanged();
+        }
     }
 }
